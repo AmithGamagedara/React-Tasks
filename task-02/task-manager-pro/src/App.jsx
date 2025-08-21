8fbc668817c83dae47f86411d8d0b8047429fc19
Amith Gamagedara <amithofficial00@gmail.com>
2025-08-21 15:52:05 +0530

    add: create board page

--- task-02/task-manager-pro/src/App.jsx
+++ task-02/task-manager-pro/src/App.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import { Card } from "antd";
-import { BrowserRoute, Routes ,Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import BoardPage from "./pages/BoardPage";
+import BoardList from "./components/BoardList";
 
 function App() {
   return (
-    <BrowserRoute>
-    <Routes>
-      <Route/>
-    </Routes>
-    </BrowserRoute>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/boards" replace />} />
+        <Route path="/boards" element={<BoardList />} />
+        <Route path="/boards/:id" element={<BoardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- task-02/task-manager-pro/src/pages/BoardPage.jsx
+++ task-02/task-manager-pro/src/pages/BoardPage.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Row, Col, Card } from "antd";
 
 function BoardPage() {
+  const { id } = useParams();
+
+  const boards = [
+    { id: 1, name: "Personal Tasks", description: "Groceries, workouts" },
+    { id: 2, name: "Team Board", description: "Sprint backlog" },
+    { id: 3, name: "Personal", description: "workouts" },
+    { id: 4, name: "Team ", description: "Sprint" },
+  ];
+
+  const board = boards.find((b) => b.id === Number(id));
+
+  const tasks = [
+    { id: 1, title: "Buy groceries", listId: "todo" },
+    { id: 2, title: "Team meeting", listId: "inprogress" },
+    { id: 3, title: "Finish project report", listId: "done" },
+  ];
+
+  const lists = [
+    { id: "todo", name: "To Do" },
+    { id: "inprogress", name: "In Progress" },
+    { id: "done", name: "Done" },
+  ];
+
   return (
-    <div>BoardPage</div>
-  )
+    <div style={{ padding: "20px" }}>
+      <h2>{board ? board.name : "Board not found"}</h2>
+      <p>{board?.description}</p>
+      <Row gutter={16}>
+        {lists.map((list) => (
+          <Col span={8} key={list.id}>
+            <Card title={list.name} style={{ minHeight: "300px" }}>
+              {tasks
+                .filter((task) => task.listId === list.id)
+                .map((task) => (
+                  <Card
+                    key={task.id}
+                    size="small"
+                    style={{ marginBottom: "8px" }}
+                  >
+                    {task.title}
+                  </Card>
+                ))}
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 }
 
-export default BoardPage
+export default BoardPage;
